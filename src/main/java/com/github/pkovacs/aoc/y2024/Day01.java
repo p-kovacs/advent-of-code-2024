11399871778a5ef44a791e96b9f5adf28364622b
Péter Kovács <kovacspeter84@gmail.com>
2024-12-04 16:19:20 +0100

    Add skeleton classes and test for year 2024

---
+++ src/main/java/com/github/pkovacs/aoc/y2024/Day01.java
@@ -0,0 +1,23 @@
+package com.github.pkovacs.aoc.y2024;
+
+import com.github.pkovacs.aoc.AbstractDay;
+
+public class Day01 extends AbstractDay {
+
+    private static final String EXAMPLE = """
+            TODO
+            """;
+
+    public static void main(String[] args) {
+        var lines =
+                EXAMPLE.lines().toList();
+//                readLines(getInputPath());
+
+        
+
+        System.out.println();
+//        System.out.println("Part 1: " + solve(lines, 1));
+//        System.out.println("Part 2: " + solve(lines, 2));
+    }
+
+}
