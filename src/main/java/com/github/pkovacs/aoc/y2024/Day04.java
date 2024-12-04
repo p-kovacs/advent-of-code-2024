35c13aa7a35e303c0640c1e7c9b297d949be7182
Péter Kovács <kovacspeter84@gmail.com>
2024-12-04 22:58:54 +0100

    Add solution for Day 4

--- src/main/java/com/github/pkovacs/aoc/y2024/Day04.java
+++ src/main/java/com/github/pkovacs/aoc/y2024/Day04.java
@@ -1,23 +1,41 @@
 package com.github.pkovacs.aoc.y2024;
 
+import java.util.List;
+
 import com.github.pkovacs.aoc.AbstractDay;
+import com.github.pkovacs.util.data.Cell;
+import com.github.pkovacs.util.data.CharTable;
 
 public class Day04 extends AbstractDay {
 
-    private static final String EXAMPLE = """
-            TODO
-            """;
-
     public static void main(String[] args) {
-        var lines =
-                EXAMPLE.lines().toList();
-//                readLines(getInputPath());
+        var table = new CharTable(readLines(getInputPath()));
 
-        
+        System.out.println("Part 1: " + table.cells().mapToLong(p -> countXmasRays(table, p)).sum());
+        System.out.println("Part 2: " + table.cells().filter(p -> isXmasCenter(table, p)).count());
+    }
 
-        System.out.println();
-//        System.out.println("Part 1: " + solve(lines, 1));
-//        System.out.println("Part 2: " + solve(lines, 2));
+    private static long countXmasRays(CharTable table, Cell p) {
+        return table.get(p) != 'X'
+                ? 0
+                : table.extendedNeighbors(p)
+                        .map(n -> table.ray(p, n).map(table::get).limit(3).toList())
+                        .filter(List.of('M', 'A', 'S')::equals)
+                        .count();
+    }
+
+    private static boolean isXmasCenter(CharTable table, Cell p) {
+        if (table.get(p) != 'A') {
+            return false;
+        }
+
+        var diagonalNeighbors = table.extendedNeighbors(p).filter(n -> n.x() != p.x() && n.y() != p.y()).toList();
+        return diagonalNeighbors.size() == 4 && diagonalNeighbors.stream().allMatch(n ->
+                (table.get(n) == 'M' || table.get(n) == 'S') && table.get(oppositeNeighbor(p, n)) != table.get(n));
+    }
+
+    private static Cell oppositeNeighbor(Cell p, Cell neighbor) {
+        return new Cell(p.y() + p.y() - neighbor.y(), p.x() + p.x() - neighbor.x());
     }
 
 }
